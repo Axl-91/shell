9a8358cd80c023d13ac6f164d79c06d18274af46
Axel Aparicio Rotermund <axelaparicior@gmail.com>
2025-04-02 19:18:23 -0300

    Added part 2

--- exec.c
+++ exec.c
@@ -1,10 +1,14 @@
 #include "exec.h"
 #include "defs.h"
 #include "freecmd.h"
+#include "parsing.h"
+#include "printstatus.h"
+#include "types.h"
 #include "utils.h"
 #include <stdlib.h>
-#include <stdlib.h>
-#include <sys/types.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
@@ -72,24 +76,149 @@ set_environ_vars(char **eargv, int eargc)
 static int
 open_redir_fd(char *file, int flags)
 {
-	// Your code here
+	int fd = open(file, flags, 0644);
 
-	return -1;
+	if (fd == -1) {
+		perror("Error");
+		return -1;
+	}
+	return fd;
 }
 
-void execute_cmd(struct execcmd* exec_cmd) {
-	pid_t pid = fork();
-	char* argsv[MAXARGS + 1] = { NULL };
+void
+execute_cmd(struct execcmd *exec_cmd)
+{
+	if (execvp(exec_cmd->argv[0], exec_cmd->argv) == -1) {
+		bool is_command = exec_cmd->scmd[0] != '.';
+		free_command((struct cmd *) exec_cmd);
 
-	for (int i = 0; i < exec_cmd->argc; i++){
-		argsv[i] = exec_cmd->argv[i];
+		if (errno == ENOENT && is_command) {
+			printf_debug("Error: Command not found\n", errno);
+		} else {
+			perror("Error");
+		}
+		exit(EXIT_FAILURE);
+	}
+}
+
+// Executes the redirection from the old_fd to the new one pointing to the file
+int
+execute_redir(char *file, int old_fd, int flags)
+{
+	pid_t new_fd;
+
+	if (strcmp(file, "&1") == 0) {
+		new_fd = STDOUT_FILENO;
+	} else if ((new_fd = open_redir_fd(file, flags)) == -1) {
+		return -1;
+	}
+	if (dup2(new_fd, old_fd) == -1) {
+		perror("Error");
+		close(new_fd);
+		return -1;
+	}
+	return 0;
+}
+
+void
+redirect_fds(struct execcmd *redir_cmd)
+{
+	char *file;
+	// Flags for output files (write only, create if not exist, truncate, close on exec).
+	int out_flags = O_WRONLY | O_CREAT | O_TRUNC | O_CLOEXEC;
+	// Flags for input files (read only, close on exec).
+	int in_flags = O_RDONLY | O_CLOEXEC;
+	// Flags for error files (write only, create if not exist, close on exec).
+	int err_flags = O_WRONLY | O_CREAT | O_CLOEXEC;
+
+	// There is a redirection for stdout
+	if (strlen(redir_cmd->out_file) > 0) {
+		file = redir_cmd->out_file;
+		if (execute_redir(file, STDOUT_FILENO, out_flags) == -1) {
+			free_command((struct cmd *) redir_cmd);
+			exit(EXIT_FAILURE);
+		}
+	}
+	// There is a redirection for stdin
+	if (strlen(redir_cmd->in_file) > 0) {
+		file = redir_cmd->in_file;
+		if (execute_redir(file, STDIN_FILENO, in_flags) == -1) {
+			free_command((struct cmd *) redir_cmd);
+			exit(EXIT_FAILURE);
+		}
+	}
+	// There is a redirection for stderr
+	if (strlen(redir_cmd->err_file) > 0) {
+		file = redir_cmd->err_file;
+		if (execute_redir(file, STDERR_FILENO, err_flags) == -1) {
+			free_command((struct cmd *) redir_cmd);
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+
+int handle_pipes(struct pipecmd *pipe_cmd) {
+	int fds[2];
+	pipe(fds);
+
+	pid_t pid_left = fork();
+
+	if (pid_left == 0) {
+		close(fds[READ]);
+		dup2(fds[WRITE], STDOUT_FILENO);
+		close(fds[WRITE]);
+		struct cmd *left_cmd = pipe_cmd->leftcmd;
+		
+		free_command(pipe_cmd->rightcmd);
+		free(pipe_cmd);
+
+		exec_cmd(left_cmd);
 	}
 
-	if (pid == 0) {
-		execvp(argsv[0], argsv);
-	} else {
-		wait(NULL);
+	struct cmd *new_right_cmd = parse_line(pipe_cmd->rightcmd->scmd);
+	bool is_pipe = new_right_cmd->type == PIPE;
+
+	pid_t pid_right = fork();
+
+	if (pid_right == 0) {
+		close(fds[WRITE]);
+		dup2(fds[READ], STDIN_FILENO);
+		close(fds[READ]);
+
+		free_command((struct cmd *) pipe_cmd);
+
+		if (!is_pipe){
+			exec_cmd(new_right_cmd);
+		} else {
+			struct pipecmd* new_pipe = (struct pipecmd *) new_right_cmd;
+			int exit_status = handle_pipes(new_pipe);
+			free_command(new_right_cmd);
+			exit(exit_status);
+		}
 	}
+
+	close(fds[READ]);
+	close(fds[WRITE]);
+	free_command(new_right_cmd);
+
+	int exit_status = EXIT_SUCCESS;
+		
+	if (waitpid(pid_left, &status, 0) != -1) {
+		print_status_info(pipe_cmd->leftcmd);
+		if (WEXITSTATUS(status) == 1) {
+			exit_status = EXIT_FAILURE;
+		}
+	}
+
+	if (waitpid(pid_right, &status, 0) != -1 ) {
+		if (!is_pipe) {
+			print_status_info(pipe_cmd->rightcmd);
+		}
+		if (WEXITSTATUS(status) == 1) {
+			exit_status = EXIT_FAILURE;
+		}
+	}
+	return exit_status;
 }
 
 // executes a command - does not return
@@ -110,10 +239,10 @@ exec_cmd(struct cmd *cmd)
 	switch (cmd->type) {
 	case EXEC:
 		// spawns a command
-		exec_cmd = (struct execcmd*) cmd;
+		//
+		// Your code here
+		exec_cmd = (struct execcmd *) cmd;
 		execute_cmd(exec_cmd);
-		free_command(cmd);
-		exit(EXIT_SUCCESS);
 		break;
 
 	case BACK: {
@@ -131,24 +260,22 @@ exec_cmd(struct cmd *cmd)
 		// To check if a redirection has to be performed
 		// verify if file name's length (in the execcmd struct)
 		// is greater than zero
-		//
-		// Your code here
-		printf("Redirections are not yet implemented\n");
-		_exit(-1);
+		redir_cmd = (struct execcmd *) cmd;
+		redirect_fds(redir_cmd);
+		execute_cmd(redir_cmd);
 		break;
 	}
 
 	case PIPE: {
 		// pipes two commands
-		//
-		// Your code here
-		printf("Pipes are not yet implemented\n");
+		pipe_cmd = (struct pipecmd *) cmd;
+		int exit_status = handle_pipes(pipe_cmd);
 
 		// free the memory allocated
 		// for the pipe tree structure
 		free_command(parsed_pipe);
 
-		break;
+		exit(exit_status);
 	}
 	}
 }
